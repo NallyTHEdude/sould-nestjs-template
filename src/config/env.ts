f5dd8bc3a28edb041ec687688c3da419457366e9
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-17 19:56:22 +0530

    fix: fixed url validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,23 @@
 import { z } from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env',
+});
+
+const urlSchema = z.string().refine(
+  (value) => {
+    try {
+      new URL(value);
+      return true;
+    } catch {
+      return false;
+    }
+  },
+  {
+    message: 'Invalid URL',
+  },
+);
 
 const envBoolean = z
   .enum(['true', 'false'])
@@ -9,7 +28,7 @@ export const configSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
-  BASE_URL: z.string().url().default('http://localhost'),
+  BASE_URL: urlSchema.default('http://localhost:4000'),
   PORT: z.coerce.number().default(4000),
 
   // Server configurations
@@ -17,7 +36,7 @@ export const configSchema = z.object({
   USE_REDIS: envBoolean.default(false),
   USE_SESSION: envBoolean.default(false),
   USE_CORS: envBoolean.default(false),
-  CORS_ORIGIN: z.string().url().default('http://localhost:3000'),
+  CORS_ORIGIN: urlSchema.default('http://localhost:3000'),
 
   // Database validations
   DATABASE_URL: z.string().url(),
