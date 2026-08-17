9579f251a52020522f833e5da3808843a9be6097
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-17 18:32:40 +0530

    feat: server configuration flags added

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
+const envBoolean = z
+  .enum(['true', 'false'])
+  .transform((value) => value === 'true');
+
 export const configSchema = z.object({
   // Environment validations
   NODE_ENV: z
@@ -7,6 +11,12 @@ export const configSchema = z.object({
     .default('development'),
   BASE_URL: z.string().url().default('http://localhost'),
   PORT: z.coerce.number().default(4000),
+
+  // Server configurations
+  USE_COOKIES: envBoolean.default(false),
+  USE_CORS: envBoolean.default(false),
+  USE_REDIS: envBoolean.default(false),
+  USE_SESSION: envBoolean.default(false),
 
   // Database validations
   DATABASE_URL: z.string().url(),
