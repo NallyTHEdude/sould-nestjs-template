a9a7cf7b86084b2bf249674e892a9e11e13b24f5
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-17 22:16:42 +0530

    chore: using databaseModule and userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { UserModule } from './_modules/user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    DatabaseModule,
+    UserModule,
     ThrottlerModule.forRoot({
       throttlers: [
         {
