2d64e293f944440a974a4810b6fd90c4bff1be21
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-18 20:30:12 +0530

    chore: removed rate limit from healthCheck endpoint

--- src/_modules/health/health.controller.ts
+++ src/_modules/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import type { HealthCheckResponseDto } from './dto/healthCheck.dto';
 import { HealthService } from './health.service';
+import { SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('/health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
