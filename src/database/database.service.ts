b59d8c8fe0e414020f0b393db735f3c9e7f8b804
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-18 17:12:56 +0530

    chore: setup winston logging

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -3,22 +3,25 @@ import { PrismaClient } from 'generated/prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import config from '@/config/index';
+import { LoggerService } from '@/logger/logger.service';
 
 @Injectable()
 export class DatabaseService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
+  constructor(private readonly logger: LoggerService) {
     const pool = new Pool({ connectionString: config.DATABASE_URL });
     const adapter = new PrismaPg(pool);
     super({ adapter });
   }
+
   async onModuleInit() {
     try {
       await this.$connect();
+      this.logger.info('Database connected successfully', DatabaseService.name);
     } catch (err) {
-      console.log('Database connection failed');
+      this.logger.error('Database connection failed:', String(err));
       throw err;
     }
   }
