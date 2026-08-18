6966fbe30c1ffda1815e613bd706aafbe5a9b598
Nawaz <rifaqatnawaz25@gmail.com>
2026-08-18 21:41:00 +0530

    feat: global validation pipeline secure setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from '@/app.module';
 import config from '@/config';
 import helmet from 'helmet';
+
+// constants for configurable options
+// provides automatic validation and transformation of incoming requests based on DTOs
+const VALIDATION_PIPE_OPTIONS = {
+  whitelist: true, // Automatically strip unannotated properties
+  forbidNonWhitelisted: true, // Throw an error if unauthorized properties are sent
+  transform: true, // Automatically transform payloads to match DTO object types
+};
+
+const CORS_OPTIONS = {
+  origin: config.CORS_ORIGIN,
+  credentials: config.USE_COOKIES,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -12,11 +26,11 @@ async function bootstrap() {
 
   // Security middleware
   app.use(helmet());
+
+  //TODO: uncomment after project completion
+  app.useGlobalPipes(new ValidationPipe(VALIDATION_PIPE_OPTIONS));
   if (config.USE_CORS) {
-    app.enableCors({
-      origin: config.CORS_ORIGIN,
-      credentials: config.USE_COOKIES,
-    });
+    app.enableCors(CORS_OPTIONS);
   }
 
   // Listen on the specified port
